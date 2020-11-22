3b0915cdbe4af0643a5d38bb341641a022815614
Alex Iskakov <ma3ca3@gmail.com>
2020-11-22 15:14:16 +0500

    cvc

--- backend/subscription/index.php
+++ backend/subscription/index.php
@@ -1,10 +1,8 @@
 <?php
-var_dump($_POST);
-
 if (!empty($_GET)) {
   $arr=[];
   $myObj = new stdClass();
   $myObj->name = 'хуй';
   $arr[] = $myObj;
-  var_dump(json_encode($arr));
+  echo json_encode($arr);
 }
